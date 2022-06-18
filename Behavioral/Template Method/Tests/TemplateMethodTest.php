b36797e827d2a45f9de90d621b02896b785d260f
REDCROSS16 <belkill@mail.ru>
2022-06-18 21:04:55 +0300

    template method

--- Behavioral/Template Method/Tests/TemplateMethodTest.php
+++ Behavioral/Template Method/Tests/TemplateMethodTest.php
@@ -3,6 +3,7 @@
 namespace DesignPatterns\Behavioral\TemplateMethod\Tests;
 
 use DesignPatterns\Behavioral\TemplateMethod\BeachJourney;
+use DesignPatterns\Behavioral\TemplateMethod\CityJourney;
 
 class TemplateMethodTest extends TestCase
 {
@@ -10,5 +11,26 @@ class TemplateMethodTest extends TestCase
     {
         $beachJourney = new BeachJourney();
         $beachJourney->takeATrip();
+
+
+        $this->assertSame(
+            ['Buy a flight ticket', 'Taking the plane', 'Swimming and sun-bathing', 'Taking the plane'],
+            $beachJourney->getThingsToDo()
+        );
+    }
+
+    public function testCanGetOnAJourneyTOACity()
+    {
+        $cityJourney = new CityJourney();
+        $cityJourney->takeATrip();
+
+        $this->assertSame([
+            'Buy a flight ticket',
+            'Taking the plane',
+            'Eat, drink, take photos and sleep',
+            'Buy a gift',
+            'Taking the plane'
+        ], 
+        $cityJourney->getThingsToDo());
     }
 }
