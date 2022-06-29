ab8447eec657892e3d7c060560352f65b0f26960
REDCROSS16 <belkill@mail.ru>
2022-06-29 12:14:50 +0300

    update visitor

--- Behavioral/Visitor/Group.php
+++ Behavioral/Visitor/Group.php
@@ -4,5 +4,17 @@ namespace DesignPatterns\Behavioral\Visitor;
 
 class Group implements Role
 {
-    
+    public function __construct(private string $name)
+    {
+    }
+
+    public function getName(): string
+    {
+        return sprintf('Group %s', $this->name);
+    }
+
+    public function accept(RoleVisitor $visitor)
+    {
+        $visitor->visitGroup($this);
+    }
 }
