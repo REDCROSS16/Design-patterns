d735589c6fa3226e5c9226a4f24c5b8e6c44bee4
REDCROSS16 <belkill@mail.ru>
2022-07-05 22:57:52 +0300

    service locator

--- Behavioral/Service Locator/ServiceLocator.php
+++ Behavioral/Service Locator/ServiceLocator.php
@@ -2,6 +2,8 @@
 
 namespace DesignPatterns\Behavioral\ServiceLocator;
 
+use InvalidArgumentException;
+
 class ServiceLocator
 {
     private array $services = [];
@@ -21,5 +23,22 @@ class ServiceLocator
     public function has(string $interface): bool
     {
         return isset($this->services[$interface]) || isset($this->instantied[$interface]);
+    }
+
+    public function get(string $class): Service
+    {
+        if (isset($this->instantied[$class])) {
+            return $this->instantied[$class];
+        }
+
+        $object = new $class(...$this->instantied[$class]);
+
+        if (!$object instanceof Service) {
+            throw new InvalidArgumentException('Could not registred');
+        }
+
+        $this->instantied[$class] = $object;
+
+        return $object;
     }
 }
