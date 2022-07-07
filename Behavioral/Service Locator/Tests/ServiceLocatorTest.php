7b1d46fc88c75073c53de08390a6f4b00eec02bf
REDCROSS16 <belkill@mail.ru>
2022-07-08 02:13:55 +0300

    service upd

--- Behavioral/Service Locator/Tests/ServiceLocatorTest.php
+++ Behavioral/Service Locator/Tests/ServiceLocatorTest.php
@@ -2,7 +2,14 @@
 
 namespace DesignPatterns\Behavioral\ServiceLocator\Tests;
 
+use DesignPatterns\Behavioral\ServiceLocator\ServiceLocator;
+
 class ServiceLocatorTest extends TestCase
 {
+    public function setup(): void
+    {
+        $this->serviceLocator = new ServiceLocator;
+    }
 
+    
 }
