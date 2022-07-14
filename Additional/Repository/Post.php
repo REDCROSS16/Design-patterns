ff2f7df4edf5e16ac3bd36a6ac8346b581e13dda
REDCROSS16 <belkill@mail.ru>
2022-07-14 23:48:30 +0300

    epository pattern

---
+++ Additional/Repository/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DesignPatterns\Additional\Repository;
+
+class Post 
+{
+    public static function draft(PostId $id, string $title, string $text): Post
+    {
+        return new self(
+            $id,
+            PostStatus::fromString(PostStatus::STATE_DRAFT),
+            $title,
+            $text
+        );
+    }
+    
+}
