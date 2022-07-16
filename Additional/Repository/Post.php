30185c6596bc2649a974e6458e207c6b16bf6429
REDCROSS16 <belkill@mail.ru>
2022-07-16 16:15:05 +0300

    update repository pattern

--- Additional/Repository/Post.php
+++ Additional/Repository/Post.php
@@ -13,5 +13,24 @@ class Post
             $text
         );
     }
+
+    public function getId(): PostId
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): PostStatus
+    {
+        return $this->status;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
     
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
 }
