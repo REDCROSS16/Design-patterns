4ff6dd27c8b106ac69d85e2472841b00f3f9d0ec
REDCROSS16 <belkill@mail.ru>
2022-07-22 00:24:39 +0300

    update

--- Additional/Repository/PostStatus.php
+++ Additional/Repository/PostStatus.php
@@ -6,4 +6,7 @@ class PostStatus
 {
     public const STATE_DRAFT_ID = 1;
     public const STATE_PUBLISHED_ID = 2;
+
+    public const STATE_DRAFT = 'draft';
+    public const STATE_PUBLISHED = 'published';
 }
