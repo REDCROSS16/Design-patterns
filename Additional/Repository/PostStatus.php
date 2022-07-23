4717dc3d621cdc8de28de3d6ec86a926a9d2418e
REDCROSS16 <belkill@mail.ru>
2022-07-23 23:48:50 +0300

    update

--- Additional/Repository/PostStatus.php
+++ Additional/Repository/PostStatus.php
@@ -9,4 +9,17 @@ class PostStatus
 
     public const STATE_DRAFT = 'draft';
     public const STATE_PUBLISHED = 'published';
+
+    private static array $validStates = [
+        self::STATE_DRAFT_ID => self::STATE_DRAFT,
+        self::STATE_PUBLISHED_ID => self::STATE_PUBLISHED
+    ];
+
+    public static function formInt(int $statusId)
+    {
+        self::ensureIsValidId($statusId);
+
+        return new self($statusId, self::$validStates[$statusId]);
+
+    }
 }
